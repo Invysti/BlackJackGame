bebf42e0faf30408968c0f6db31f6e7ee89d2011
Invysti <invysti@gmail.com>
2017-08-09 13:27:31 -0500

    Created eclipse project and added preliminary code.

---
+++ BlackJackGame.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.Math;
+
+public class BlackJackGame {
+	public static final int CARDS = 52;
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		intro(keyboard);
+		getRounds(keyboard);
+		
+		
+	}
+	
+	// Prints out the intro to the game.
+	public static void intro(Scanner keyboard) {
+		System.out.print("Welcome to BlackJack! Please enter your name: ");
+		String playerName = keyboard.nextLine();
+		System.out.print("Hello " + playerName + ". Let's begin!");
+	}
+	
+	// Gets the number of rounds the user would like to play.
+	public static int getRounds(Scanner keyboard) {
+		int rounds;
+		System.out.println("How many rounds from 1 to 10 would you like to play?");
+		do {
+			System.out.print("Please enter an amount between 1 and 10: ");
+			while (!keyboard.hasNextInt()) {
+				System.out.print("Please enter an integer: ");
+				keyboard.next();
+			}
+			rounds = keyboard.nextInt();
+		} while (rounds < 1 || rounds > 10);
+		return rounds;
+	}
+	
+}
